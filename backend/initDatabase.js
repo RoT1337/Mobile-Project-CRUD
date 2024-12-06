0f5409285e57776b2f5fc5ffa948d2adeee0ced2
Rob <120913030+RoT1337@users.noreply.github.com>
2024-12-06 10:35:40 +0800

    Update initDatabase.js
    Added habits table for initialization

--- backend/initDatabase.js
+++ backend/initDatabase.js
@@ -9,16 +9,31 @@ const db = new sqlite3.Database('./database.db', (err) => {
 });
 
 db.serialize(() => {
-  db.run(`CREATE TABLE IF NOT EXISTS users (
+  db.run(
+    `CREATE TABLE IF NOT EXISTS users (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT NOT NULL,
     email TEXT NOT NULL UNIQUE,
     password TEXT NOT NULL
-  )`, (err) => {
+    )`, (err) => {
     if (err) {
       console.error('Error creating users table:', err.message);
     } else {
       console.log('Users table created successfully.');
+    }
+  });
+
+  db.run(
+    `CREATE TABLE IF NOT EXISTS habits (
+    name TEXT NOT NULL,
+    description TEXT NOT NULL,
+    category TEXT NOT NULL,
+    frequency TEXT NOT NULL
+    )`, (err) => {
+    if (err) {
+      console.error('Error creating habits table:', err.message);
+    } else {
+      console.log('Habits table created successfully');
     }
   });
 });
